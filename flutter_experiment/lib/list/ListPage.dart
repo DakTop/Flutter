8949dc58c7d515731e500f84bbec6b00cb922ed5
e <zengliang.ba@xiaoyeyun.com>
2019-04-11 19:31:25 +0800

    列表页优化

--- flutter_experiment/lib/list/ListPage.dart
+++ flutter_experiment/lib/list/ListPage.dart
@@ -3,40 +3,46 @@ import 'package:flutter_experiment/resources/AppColors.dart';
 import 'package:flutter_experiment/list/children/ListChildPage.dart';
 
 class ListPage extends StatelessWidget {
-  static List<String> listTab = <String>[
-    "列表",
-    "列表",
-    "动画",
+
+  Widget _tabBarContainer;
+
+  List<Tab> _listTab = <Tab>[
+    new Tab(
+      text: "列表",
+    ),
+    new Tab(
+      text: "列表",
+    ),
+    new Tab(
+      text: "动画",
+    )
   ];
 
-  Widget tabBarContainer = new TabBar(
-    tabs: listTab.map((String tabStr) {
-      return new Tab(
-        text: tabStr,
-      );
-    }).toList(),
-    indicatorColor: AppColors.color_a0522d,
-    isScrollable: true,
-    indicatorWeight: 2.0,
-  );
+  ListPage() {
+    _tabBarContainer = new TabBar(
+      tabs: _listTab,
+      indicatorColor: AppColors.color_ffffff,
+      isScrollable: true,
+      indicatorWeight: 2.0,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new DefaultTabController(
-      length: 3,
+      length: _listTab.length,
       initialIndex: 0,
       child: new Column(
         children: <Widget>[
           new Container(
             constraints: new BoxConstraints.expand(
               height:
-                  Theme.of(context).textTheme.display1.fontSize * 1.1 + 50.0,
+                  Theme.of(context).textTheme.display1.fontSize * 1.1 + 10.0,
             ),
-            padding: const EdgeInsets.all(10),
             alignment: Alignment.center,
             color: AppColors.color_a0522d,
-            child: tabBarContainer,
+            child: _tabBarContainer,
           ),
           new Expanded(child: new ListChildPage())
         ],
--- flutter_experiment/lib/resources/AppColors.dart
+++ flutter_experiment/lib/resources/AppColors.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color color_a0522d = const Color(0xFFA0522D);
+  static Color color_a0522d = const Color(0xFFF08080);
+  static Color color_ffffff = const Color(0xFFFFFFFF);
 }
