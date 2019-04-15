b3fc12c06bd4d1aa63b4866f14350ee9a49c8435
runTop <bzlup.139.com>
2019-04-16 00:27:51 +0800

    ListView列表

--- flutter_experiment/lib/list/children/ListChildPage.dart
+++ flutter_experiment/lib/list/children/ListChildPage.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
 
 class ListChildPage extends StatelessWidget {
+  List<String> _list = new List<String>.generate(10, (i) {
+    return '$i' 'item';
+  });
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
-      child: Text(
-        "列表",
-        textDirection: TextDirection.ltr,
-      ),
+    return Column(
+      children: <Widget>[
+        Image(
+          image: new AssetImage('images/list_head.jpg'),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: _list.length, //列表长度
+            itemExtent: 50.0,//每一个item的高，如果不设置则依赖子控件的高度
+            itemBuilder: (context, position) {
+              return Text('这是第' + _list.elementAt(position));
+            }, //返回item子控件
+          ),
+        ),
+      ],
     );
   }
 }
+
+
