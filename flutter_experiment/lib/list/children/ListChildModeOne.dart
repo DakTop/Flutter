4d874da42a5e6851e335a246ba3cb746fecb145e
e <zengliang.ba@xiaoyeyun.com>
2019-04-18 19:32:28 +0800

    提交简单列表

--- flutter_experiment/lib/list/children/ListChildModeOne.dart
+++ flutter_experiment/lib/list/children/ListChildModeOne.dart
@@ -1,32 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_experiment/resources/AppColors.dart';
 
+/**
+ * 通过这总构造函数创建的ListView布局只是适合拥有少量item的布局，因为当加载这样的ListView时候，需要将全部的item创建出来，
+ * 并不是显示哪些item就创建哪些item。它的效果和使用SingleChildScrollView+Column的方式没有本质的区别。
+ */
 class ListChildModeOne extends StatelessWidget {
-  List<String> _list = new List<String>.generate(10, (i) {
-    return '$i' 'item';
-  });
+  List<String> _listText = <String>[
+    "item0",
+    "item1",
+    "item2",
+    "item3",
+    "item4",
+    "item5",
+    "item6",
+    "item7",
+    "item8",
+  ];
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Column(
-      children: <Widget>[
-        Image(
-          image: new AssetImage('images/list_head.jpg'),
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: _list.length, //列表长度
-            itemExtent: 60.0,//每一个item的高，如果不设置则依赖子控件的高度
-            itemBuilder: (context, position) {
-              return Text('这是第' + _list.elementAt(position),
-
-              );
-            }, //返回item子控件
-          ),
-        ),
-      ],
+    return Container(
+      color: AppColors.color_00ffffff,
+      child: ListView(
+        scrollDirection: Axis.vertical, //滚动方向
+        itemExtent: 100.0, //每一个item的高，如果不设置则依赖子控件的高度
+        children: _listText.map((String text) {
+          return Text(
+            text,
+            style: TextStyle(
+              color: AppColors.color_3b3b3b,
+              fontSize: 30.0,
+              decoration: TextDecoration.none, //去掉下划线
+            ),
+          );
+        }).toList(),
+      ),
     );
   }
 }
-
-
---
+++ flutter_experiment/lib/list/children/ListChildModeTwo.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ListChildModeOne extends StatelessWidget {
+  List<String> _list = new List<String>.generate(10, (i) {
+    return '$i' 'item';
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: <Widget>[
+        Image(
+          image: new AssetImage('images/list_head.jpg'),
+        ),
+        Expanded(
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,//滚动方向
+            itemCount: _list.length, //列表长度
+            itemExtent: 60.0,//每一个item的高，如果不设置则依赖子控件的高度
+            itemBuilder: (context, position) {
+              return Text('这是第' + _list.elementAt(position),
+
+              );
+            }, //返回item子控件
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+
